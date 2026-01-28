e836db23fc068924d0a5334c70224f81b0333f5e
Manish-SDU <maraj24@student.sdu.dk>
2026-01-29 00:19:39 +0100

    added correct ble key

--- LevelUp_App/config/wifi2ble.php
+++ LevelUp_App/config/wifi2ble.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'base_url' => env('WIFI2BLE_BASE_URL', 'http://simulator:8000'),
+    'base_url' => env('WIFI2BLE_BASE_URL', env('WIFI2BLE_HOST') ? 'https://' . env('WIFI2BLE_HOST') : 'http://simulator:8000'),
     'api_key' => env('WIFI2BLE_API_KEY'),
     'timeout' => (int) env('WIFI2BLE_TIMEOUT', 5),
 ];
